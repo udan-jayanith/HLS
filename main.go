a4873f3be1a21d4f4bb9ed783a9c768f826d2c86
Udan Jayanith Jayakody <j.udanjayanith@gmail.com>
2025-10-25 17:32:16 +0530

    Fixed golang module description

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 //HLS Go module implements HTTP Live Streaming protocol for Go.
-//HLS can can encode and decode HTTP Live Stream and also provide a tokenizer and a serializer for low level access.
+//HLS can encode and decode HTTP Live Streams and also provide a tokenizer and a serializer for low level access.
 //This HLS module does not serve HTTP live streams and it's users responsibility to serve HTTP live streams.
 //But HLS provides helper methods to server HTTP live streams.
 
