c6659278aacdee273f74b07372452c34e2e90e8c
Udan Jayanith Jayakody <j.udanjayanith@gmail.com>
2025-10-26 12:56:25 +0530

    tests

---
+++ attribute-types_test.go
@@ -0,0 +1,38 @@
+package HLS_test
+
+import "testing"
+
+func TestIsDecimalInteger(t *testing.T) {
+
+}
+
+func TestIsHexadecimalSequence(t *testing.T) {
+
+}
+
+func TestDecimalFloatingPoint(t *testing.T) {
+
+}
+
+func TestSignedDecimalFloatingPoint(t *testing.T) {
+
+}
+
+func TestIsString(t *testing.T) {
+
+}
+
+func TestQuotedString(t *testing.T) {
+
+}
+
+func TestIsEnumeratedString(t *testing.T) {
+
+}
+
+func TestIsDecimalResolution(t *testing.T) {
+
+}
+func TestWrapQuotes(t *testing.T) {
+
+}
