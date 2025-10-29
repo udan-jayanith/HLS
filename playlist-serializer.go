f6069349fd6d36440b86dd700c73428e2507725d
Udan Jayanith Jayakody <j.udanjayanith@gmail.com>
2025-10-29 20:18:08 +0530

    Add Layout

---
+++ playlist-serializer.go
@@ -0,0 +1,10 @@
+package HLS
+
+//Implement PlaylistSerializer struct
+//Add Write method to it.
+//Add AddLine method to it.
+//Add AddLineTo.
+/*
+Implement:
+	NewPlaylistToken() PlaylistToken
+*/
