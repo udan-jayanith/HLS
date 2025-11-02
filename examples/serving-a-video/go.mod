d0fc74a53788eccd3d895f6251124508d49064e7
Udan Jayanith Jayakody <j.udanjayanith@gmail.com>
2025-11-02 18:56:24 +0530

    Add a example

---
+++ examples/serving-a-video/go.mod
@@ -0,0 +1,5 @@
+module video-server
+
+go 1.25.0
+
+require github.com/udan-jayanith/HLS v0.0.0-beta.0 // indirect
--- playlist-serializer_test.go
+++ playlist-serializer_test.go
@@ -5,8 +5,6 @@ import (
 	"io"
 	"testing"
 
-	"net/http"
-
 	"github.com/udan-jayanith/HLS"
 )
 
@@ -127,65 +125,4 @@ func TestPlaylist_SetHeader(t *testing.T) {
 	if _, err := tokenizer.Advance(); err == nil {
 		t.Fatal("Expected error io.EOF but got nil")
 	}
-}
-
-func ExamplePlaylist() {
-	//This function serves a trailer to a movie in a media-playlist file.
-	http.HandleFunc("/watch/025252/dracula-trailer", func(w http.ResponseWriter, r *http.Request) {
-		playlist := HLS.NewPlaylist()
-
-		//SetHeader append tag EXTM3U and EXT_X_VERSION. 7 is the version.
-		err := playlist.SetHeader(7)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-
-		{
-			if err := playlist.AppendTag(HLS.HLSTag{
-				TagName: HLS.EXTINF,
-				Value:   "10.1",
-			}); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-
-			if err := playlist.AppendLine(HLS.NewPlaylistToken(HLS.RelativeURI, "/pt1.ts")); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-		}
-
-		{
-			if err := playlist.AppendTag(HLS.HLSTag{
-				TagName: HLS.EXTINF,
-				Value:   "9",
-			}); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-
-			if err := playlist.AppendLine(HLS.NewPlaylistToken(HLS.RelativeURI, "/pt2.ts")); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-		}
-
-		{
-			if err := playlist.AppendTag(HLS.HLSTag{
-				TagName: HLS.EXTINF,
-				Value:   "30.5",
-			}); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-
-			if err := playlist.AppendLine(HLS.NewPlaylistToken(HLS.RelativeURI, "/pt3.ts")); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-		}
-
-		// Playlist must be closed before start reading.
-		playlist.Close()
-
-		rd := bufio.NewReader(&playlist)
-		rd.WriteTo(w)
-	})
-
-	// Serves MPEG-4(.ts) for dracula-trailer.
-	// ...
 }
